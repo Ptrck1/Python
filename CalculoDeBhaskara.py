8facd9de1968426d0398c58e36ebd15bf51cc72a
Ptrck1 <90718909+Ptrck1@users.noreply.github.com>
2022-11-22 15:22:29 -0300

    Add files via upload

---
+++ CalculoDeBhaskara.py
@@ -0,0 +1,9 @@
+a = 1
+b = 2
+c = -15
+delta = (b*b - 4 * a * c)
+print (delta)
+x1 = - b + (delta**(1/2)) / 2 * a
+print (x1)
+x2 = -b - (delta**(1/2)) / 2 * a
+print (x2)
---
+++ PythonOperações.py
@@ -0,0 +1,7 @@
+a = 10
+b = 5
+
+print (a+b)
+print (a-b)
+print (a/b)
+print (a*b)
